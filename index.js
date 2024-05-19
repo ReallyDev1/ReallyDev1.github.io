6aa08705692e6523f794995f2d25601cb53504dc
TokyoLols <133028307+ReallyDev1@users.noreply.github.com>
2024-05-18 20:58:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function HomeButton() {
+    document.getElementById("title").scrollIntoView({behavior: 'smooth'})
+    console.log("Going to the home");
+}
+
+function PortfolioButton() {
+    document.getElementById("portfolio").scrollIntoView({behavior: 'smooth'})
+    console.log("Going to the portfolio");
+}
+
+function StudiosButton() {
+    document.getElementById("studios").scrollIntoView({behavior: 'smooth'})
+    console.log("Going to the studios");
+}
+
+function ContactButton() {
+    document.getElementById("contacts").scrollIntoView({behavior: 'smooth'})
+    console.log("Going to the contacts");
+}
